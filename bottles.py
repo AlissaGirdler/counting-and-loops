b530139de1ab7078140dedd4ce01e84d5443f806
Alissa Girdler <alissa.girdler@gmail.com>
2024-02-09 19:09:06 -0600

    Different verse at 5 bottles

--- bottles.py
+++ bottles.py
@@ -1,7 +1,13 @@
 for i in range (100,2,-1):
     print (i, "bottles of beer on the wall")
-    print(i, "bottles of beer")
-    print ("Take one down, pass it around", i-1, "bottles of beer on the wall..." )
+
+    if i == 5:
+        print(i, "bottles of beer")
+        print("If one of those bottles should happen to fall", i-1, "bottles of beer on the wall...")
+    else:
+        print(i, "bottles of beer")
+        print ("Take one down, pass it around", i-1, "bottles of beer on the wall...")
+
 print("2 bottles of beer on the wall\n2 bottles of beer\nTake one down pass it around 1 bottle of beer on the wall")
 print("1 bottle of beer on the wall\n1 bottle of beer\nTake one down, pass it around no more botttles beer on the wall")
 print("No more bottles of beer on the wall, no more bottles of beer.")
