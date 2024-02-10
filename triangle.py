280439e7563d35c4503858cbf436e8703f8e647a
Cole Phillips <jphillips@wisc.edu>
2024-02-09 22:01:35 -0600

    cp mods

--- triangle.py
+++ triangle.py
@@ -3,4 +3,4 @@ size = input("Enter the size of the triangle: ")
 for i in range(1, int(size) + 1):  # Outer loop for rows
     for j in range(i):  # Inner loop for columns
         print("*", end="")
-    print("")  # New line after each row 
+    print("")  # New line after each row
