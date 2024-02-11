59590011d9c7120855ad149cf96dad5685eb94ec
Cole Phillips <jphillips@wisc.edu>
2024-02-11 14:24:26 -0600

    cp mods

--- rectangle.py
+++ rectangle.py
@@ -4,4 +4,4 @@ width = input("Enter the width of the rectangle: ")
 for i in range(int(width)):  # Outer loop for rows
     for i in range(int(length)):  # Inner loop for columns
         print("*", end="")
-    print("")  # New line after each row
+    print("")  
