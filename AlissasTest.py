a97dc3f02ce0550aaa295e69888be6b467633c15
Alissa Girdler <alissa.girdler@gmail.com>
2024-02-12 21:34:15 -0600

    Random working

--- AlissasTest.py
+++ AlissasTest.py
@@ -1,16 +1,17 @@
 import random
 import sys
+
 # Flashcard questions and answers
 cards = {
-    "Q1": {"question": "Name the three different types of colors.", "answer": "Primary, secondary, and tertiary."},
-    "Q2": {"question": "List the primary colors.", "answer": "Red, yellow, and blue."},
-    "Q3": {"question": "List the secondary colors.", "answer": "Green, orange, and purple."},
-    "Q4": {"question": "How do you make secondary colors?", "answer": "Combine two primary colors."},
+    "Q1": {"question": "Name one of the three different types of color groups.", "answer": ["primary", "secondary", "tertiary"]},
+    "Q2": {"question": "List one of the primary colors.", "answer": ["red", "yellow", "blue"]},
+    "Q3": {"question": "List one of the secondary colors.", "answer": ["green", "orange", "purple"]},
+    "Q4": {"question": "How do you make secondary colors?", "answer": ["combine","mix","two","secondar"]},
     "Q5": {"question": "Name the cool primary color.", "answer": "Blue"},
-    "Q6": {"question": "Name the warm secondary color.", "answer": "Orange"},
+    "Q6": {"question": "Name the warm secondary color.", "answer": "orange"},
     "Q7": {"question": "What is another name for tertiary colors?", "answer": "Intermediate"},
     "Q8": {"question": "What is the relationship between colors on the opposite side of the color wheel?", "answer": "Complimentary"},
-    "Q9": {"question": "Name one measurement of color properties.", "answer": "Saturation" or "value"},
+    "Q9": {"question": "Name one measurement of color properties.", "answer": ["Saturation", "value"]},
     "Q10": {"question": "What color contains all colors?", "answer": "White"}
 }
 
@@ -20,16 +21,20 @@ print("Type 'yes' to get started or 'quit' to exit at any time.")
 start = input("Are you ready? ")
 
 if start.lower() == "yes":
-    while True:
         rq = random.choice(list(cards.keys()))
         print("\nQuestion: ", cards[rq]["question"])
         user_answer = input("Your answer: ")
 
-        if user_answer.lower() == random_card[4].lower():
+        while True:
+            if (user_answer.lower() not cards[rq]["answer"]):
             print("Correct!")
             break
-        else:
+        elif ()
             print("Incorrect")
             user_answer = input("Try again: ")
+else:
+    print("Bye!")
+
+
 else:
     print("Bye!")
